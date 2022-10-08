7f02f61dc043b91935354ca1161f697045bba3ef
Syafiqjos <syafiqjos@gmail.com>
2022-10-08 20:13:54 +0700

    feat: add DES algorithm base implementation

---
+++ Algorithms/DES.py
@@ -0,0 +1,11 @@
+from Algorithms.Algorithm import Algorithm
+
+class DES(Algorithm):
+    def __init__(self):
+        pass
+
+    def encrypt(self, plaintext, key):
+        pass
+
+    def decrypt(self, ciphertext, key):
+        pass
