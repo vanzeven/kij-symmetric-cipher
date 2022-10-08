386ae1b5b3ef0884420a2bc65504fcfbb5250e7a
Syafiqjos <syafiqjos@gmail.com>
2022-10-08 20:13:16 +0700

    feat: add operation base class

---
+++ Operations/Operation.py
@@ -0,0 +1,9 @@
+class Operation:
+    def __init__(self):
+        pass
+
+    def encrypt(self, algorithmClass, key, iv):
+        pass
+
+    def decrypt(self, algorithmClass, key, iv):
+        pass
