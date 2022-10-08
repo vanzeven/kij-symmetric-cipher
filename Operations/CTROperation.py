ab35e44d262d1cdc40c6c25b1ea23c658ffcd422
Syafiqjos <syafiqjos@gmail.com>
2022-10-08 20:14:22 +0700

    feat: add CTR Operation base implementation

---
+++ Operations/CTROperation.py
@@ -0,0 +1,11 @@
+from Operations.Operation import Operation
+
+class CTROperation(Operation):
+    def __init__(self):
+        pass
+
+    def encrypt(self, algorithmClass, key, iv):
+        pass
+
+    def decrypt(self, algorithmClass, key, iv):
+        pass
