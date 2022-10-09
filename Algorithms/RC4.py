57fa8bf95e1598c212861bc1283b4bbd0961a25c
Faisal Reza <7farema@gmail.com>
2022-10-09 23:29:13 +0700

    init rc4

---
+++ Algorithms/RC4.py
@@ -0,0 +1,10 @@
+class RC4:
+    def __init__(self):
+        pass
+
+    def encrypt(self, plaintext, key):
+        ciphertext = "tes " + plaintext + key
+        return ciphertext
+
+    def decrypt(self, ciphertext, key):
+        pass
--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import binascii
 from Algorithms.DES import DES
+from Algorithms.RC4 import RC4
 from Crypto.Cipher import AES
 from Operations.ECBOperation import ECBOperation
 from Operations.OFBOperation import OFBOperation
@@ -37,6 +38,10 @@ iv = pad(b"randomiv", AES.block_size)
 CFBchipertext = cfb.encrypt('AES', 'thisisplaintext', key, iv)
 
 print('CFB Cipher Text: ', CFBchipertext)
+
+# RC4
+RC4ciphertext = RC4().encrypt('plaintex', 'keyaakey')
+print('RC4 Cipher Text: ', RC4ciphertext)
 
 # Decrypt
 CFBplaintext = cfb.decrypt('AES', CFBchipertext, key, iv)
