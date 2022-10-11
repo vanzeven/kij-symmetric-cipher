90dd1ea751c16b6cc506aac57ea56139f3d23277
Syafiqjos <syafiqjos@gmail.com>
2022-10-11 23:14:17 +0700

    fix: update aes ctr

--- Algo_Operations/AES_CTR.py
+++ Algo_Operations/AES_CTR.py
@@ -1,22 +1,34 @@
+import os
+
 from Crypto.Cipher import AES
-from Crypto.Cipher import DES
 from Crypto.Util.Padding import pad
 from Crypto.Util.Padding import unpad
+from Crypto.Util import Counter
 
 class AES_CTR:
     def __init__(self, key, iv):
+        sum_ctr = 0
+        for i in iv:
+            sum_ctr += i
+
         self.key = pad(key, AES.block_size)
-        self.iv = pad(iv, AES.block_size)
+        self.iv = None
+        self.ctr = sum_ctr
+
+        self.counter = Counter.new(128, initial_value=self.ctr)
+
+    def counter(self):
+        return 3
         
     def encrypt(self, plaintext):
-        #data_bytes = bytes(plaintext, 'utf-8')
         padded_bytes = pad(plaintext, AES.block_size)
-        AES_obj = AES.new(self.key, AES.MODE_CTR, self.iv)
+        AES_obj = AES.new(self.key, AES.MODE_CTR, counter=self.counter)
         chipertext = AES_obj.encrypt(padded_bytes)
         return chipertext
 
     def decrypt(self, ciphertext):
-        AES_obj = AES.new(self.key, AES.MODE_CTR, self.iv)
+        ctr = Counter.new(128, initial_value=38)
+        AES_obj = AES.new(self.key, AES.MODE_CTR, counter=self.counter)
         raw_bytes = AES_obj.decrypt(ciphertext)
         extracted_bytes = unpad(raw_bytes, AES.block_size)
         return extracted_bytes  
