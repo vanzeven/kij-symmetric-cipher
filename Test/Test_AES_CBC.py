5c763ab1479050a35a2f4f2aef3942935ee05d96
Faisal Reza <7farema@gmail.com>
2022-10-12 01:26:36 +0700

    csv v2 init

--- Test/Test_AES_CBC.py
+++ Test/Test_AES_CBC.py
@@ -20,23 +20,26 @@ writer = Writer()
 
 ciphertext=None
 total_time = 0
-counter = 0
+writer.insert_to_array(0, "encrypt")
+counter = 1
 for i in range(10):
     start_time = time.time()
     for loop in range(1000):
         encryptor = AES_CBC(key = b'sapisapi',iv = b'47433474')
         ciphertext = encryptor.encrypt(b'abcdefghijklmnopqrstuvwxyz1234567890ABCDEFGHIJKLMNOPQRSTUVWXYZ' * 256)
     interval_time = (time.time() - start_time)
-    print("Waktu interval ke-%d adalah %s detik" % (i,interval_time))
-    writer.insert_to_array(counter, "Waktu interval ke-%d adalah %s detik" % (i,interval_time))
+    print("Waktu interval ke-%d adalah %s detik" % (i, interval_time))
+    writer.insert_to_array(counter, interval_time)
     counter += 1
     total_time += interval_time
 
 average_time = total_time/10
 
-print("Waktu rata-rata enkripsi adalah %s detik" % average_time )
-writer.insert_to_array(counter, "Waktu rata-rata enkripsi adalah %s detik" % average_time)
-counter += 1
+print("Waktu rata-rata enkripsi adalah %s detik" % average_time)
+writer.insert_to_array(counter, average_time)
+writer.write_csv("AES_CBC.csv")
+writer.insert_to_array(0, "decrypt")
+counter = 1
 
 plaintext=None
 total_time = 0
@@ -48,14 +51,12 @@ for i in range(10):
         #print(encryptor.encrypt(b'aaaa'))
     interval_time = (time.time() - start_time)
     print("Waktu interval ke-%d adalah %s detik" % (i,interval_time))
-    writer.insert_to_array(counter, "Waktu interval ke-%d adalah %s detik" % (i, interval_time))
+    writer.insert_to_array(counter, interval_time)
     counter += 1
     total_time += interval_time
 
 average_time = total_time/10
 
 print("Waktu rata-rata dekripsi adalah %s detik" % average_time )
-writer.insert_to_array(counter, "Waktu rata-rata deskripsi adalah %s detik" % average_time)
-counter += 1
-
+writer.insert_to_array(counter, average_time)
 writer.write_csv("AES_CBC.csv")
