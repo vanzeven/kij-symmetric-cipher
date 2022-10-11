cb7ce4c408820657c1c87f0393e48bb91a3a2a9a
Syafiqjos <syafiqjos@gmail.com>
2022-10-11 23:14:43 +0700

    feat: add aes cbc

---
+++ Algo_Operations/AES_CBC.py
@@ -0,0 +1,21 @@
+from Crypto.Cipher import AES
+from Crypto.Util.Padding import pad
+from Crypto.Util.Padding import unpad
+
+class AES_CBC:
+    def __init__(self, key, iv):
+        self.key = pad(key, AES.block_size)
+        self.iv = pad(iv, AES.block_size)
+
+    def encrypt(self, plaintext):
+        padded_bytes = pad(plaintext, AES.block_size)
+        AES_obj = AES.new(self.key, AES.MODE_CBC, self.iv)
+        chipertext = AES_obj.encrypt(padded_bytes)
+        return chipertext
+
+    def decrypt(self, ciphertext):
+        AES_obj = AES.new(self.key, AES.MODE_CBC, self.iv)
+        raw_bytes = AES_obj.decrypt(ciphertext)
+        extracted_bytes = unpad(raw_bytes, AES.block_size)
+        return extracted_bytes
+        
