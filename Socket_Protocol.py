bd46f279b6c5c9a25ff9175055e0fdb19b001b3b
Syafiqjos <syafiqjos@gmail.com>
2022-10-11 13:57:16 +0700

    fix: fix shlex split neverending bug

--- Socket_Protocol.py
+++ Socket_Protocol.py
@@ -23,15 +23,21 @@ class FileProtocol:
     def __init__(self):
         self.file = FileInterface()
     def proses_string(self,string_datamasuk=''):
-        logging.warning(f"string diproses: {string_datamasuk}")
-        c = shlex.split(string_datamasuk)
+        # logging.warning(f"string diproses: {string_datamasuk}")
+        logging.warning(f"string diproses: data masuk")
+
+        c = string_datamasuk.split()
         try:
             c_request = c[0].strip()
             logging.warning(f"memproses request: {c_request}")
             params = [x for x in c[1:]]
-            cl = getattr(self.file,c_request.lower())(params)
+
+            func = getattr(self.file,c_request.lower())
+
+            cl = func(params)
             return json.dumps(cl)
-        except Exception:
+        except Exception as e:
+            print(e.message)
             return json.dumps(dict(status='ERROR',data='request tidak dikenali'))
 
 
