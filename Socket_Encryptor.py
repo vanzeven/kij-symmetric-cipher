0fc642cf593415c2ddba2e0c10141390b60640c9
Syafiqjos <syafiqjos@gmail.com>
2022-10-11 15:05:06 +0700

    feat: add socket encryptor

---
+++ Socket_Encryptor.py
@@ -0,0 +1,23 @@
+from Algorithms.DES import DES
+from Algorithms.RC4 import RC4
+
+from Operations.ECBOperation import ECBOperation
+from Operations.CTROperation import CTROperation
+from Operations.OFBOperation import OFBOperation
+from Operations.CFBOperation import CFBOperation
+
+class SocketEncyrptor:
+    def __init__(self, algorithm, operation, key, iv):
+        self.algorithm = algorithm
+        self.operation = operation
+        self.key = key
+        self.iv = iv
+
+    def encrypt(self, plainbytes):
+        return self.operation.encrypt(self.algorithm, plainbytes, self.key, self.iv)
+
+    def decrypt(self, cipherbytes):
+        return self.operation.decrypt(self.algorithm, cipherbytes, self.key, self.iv)
+
+def GetSocketEncryptor():
+    return SocketEncyrptor(algorithm = DES(), operation = CTROperation(), key = 'sapiunta', iv = 4743744)
