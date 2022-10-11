6fb053bf650fcc5c655c6dc4120c2f7bc1695814
Syafiqjos <syafiqjos@gmail.com>
2022-10-11 23:58:51 +0700

    feat: add encrypt decrypt test

--- test.py
+++ test.py
@@ -1,4 +1,9 @@
 from Socket_Encryptor import GetSocketEncryptor
 
 encryptor = GetSocketEncryptor()
-print(encryptor.encrypt(b'plaintesplaintesplaintesplaintesplaintesplaintesplaintesplaintesplaintesplaintesplaintesplaintes'))
+plaintext = b'is a playable Pyro character. She is the daughter and the current owner of Fireworks. With her colorful fireworks and outgoing personality, loved by everyone on Island, as they believe summer is not the same without her.'
+ciphertext = encryptor.encrypt(plaintext)
+decryptedtext = encryptor.decrypt(ciphertext)
+
+print(ciphertext)
+print(decryptedtext)
