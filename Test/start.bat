0faf391c9a19802d51bea8dca26301e59e8e810d
AfamDafam <56785023+afamdafam@users.noreply.github.com>
2022-10-12 06:12:36 +0700

    csv for test results

---
+++ Test/start.bat
@@ -0,0 +1,11 @@
+python Test_AES_CBC.py
+python Test_AES_CFB.py
+python Test_AES_CTR.py
+python Test_AES_ECB.py
+python Test_AES_OFB.py
+
+python Test_DES_CBC.py
+python Test_DES_CFB.py
+python Test_DES_CTR.py
+python Test_DES_ECB.py
+python Test_DES_OFB.py
